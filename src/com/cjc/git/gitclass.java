b8a7c41e8321eb317c94f67a57faa97bbad24898
user <user@DESKTOP-M1237OQ>
2022-10-17 16:02:31 +0530

    add more statement

--- src/com/cjc/git/gitclass.java
+++ src/com/cjc/git/gitclass.java
@@ -5,7 +5,12 @@ public class gitclass {
 	public static void main(String[] args) {
 		
 		System.out.println("This is Git");
-		System.out.println("dghd");
+		System.out.println("Add 1");
+	}
+	
+	private void m1() {
+	 System.out.println("This is m1");
+
 	}
 
 }
